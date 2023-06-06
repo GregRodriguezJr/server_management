4c82cc86f33c36324af3d0e2786165b7456e4ba3
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-06-06 17:48:49 -0500

    completed exercise 2-1

--- ch02_ex1_sol/display_discount.php
+++ ch02_ex1_sol/display_discount.php
@@ -1,3 +1,6 @@
+<!--Greg Rodriguez-->
+<!--Assignment 2-1-->
+
 <?php
 	$product_description = filter_input(INPUT_POST, 'product_description');
 	$list_price = filter_input(INPUT_POST, 'list_price');
@@ -5,11 +8,24 @@
 	
         $discount = $list_price * $discount_percent * .01;
         $discount_price = $list_price - $discount;
+
+        // sales tax rate
+        $salesTaxRate = .08;
+        // Sales tax calculation
+        $salesTax = $discount_price * $salesTaxRate;
+        // Post tax price
+        $postTaxPrice = $discount_price - $salesTax;
         
         $list_price_f = "$".number_format($list_price, 2);
         $discount_percent_f = $discount_percent."%";
         $discount_f = "$".number_format($discount, 2);
         $discount_price_f = "$".number_format($discount_price, 2);
+        // format sales tax rate
+        $salesTaxRate_f = ($salesTaxRate * 100)."%";
+        // format sales tax amount
+        $salesTax_f = "$".number_format($salesTax, 2);
+        // format post tax price
+        $postTaxPrice_f = "$".number_format($postTaxPrice,2)
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,7 +36,7 @@
 <body>
     <main>
         <h1>Product Discount Calculator</h1>
-
+        <hr>
         <label>Product Description:</label>
         <span><?php echo htmlspecialchars($product_description); ?></span><br>
 
@@ -35,6 +51,17 @@
 
         <label>Discount Price:</label>
         <span><?php echo $discount_price_f; ?></span><br>
+
+        <!--render results-->
+        <label>Sales Tax Rate:</label>
+        <span><?php echo $salesTaxRate_f; ?></span><br>
+
+        <label>Sales Tax Amount:</label>
+        <span><?php echo $salesTax_f; ?></span><br>
+
+        <label>Total Price:</label>
+        <span><?php echo $postTaxPrice_f; ?></span><br>
+
     </main>
 </body>
 </html>
