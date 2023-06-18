802a9aa13b079956305d920ae14b0408ad85e895
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-06-18 12:30:31 -0500

    added starter files for project 6

---
+++ ch06/project_start/tech_support/view/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>SportsPro Technical Support</title>
+    <!-- This method doesnt load css had to use php include to load css -->
+    <!-- <link rel="stylesheet" type="text/css"
+          href="/project_start/tech_support/main.css"> -->
+    <style>
+        <?php include 'main.css' ?>
+    </style>
+</head>
+
+<!-- the body section -->
+<body>
+<header>
+    <h1>SportsPro Technical Support</h1>
+    <p>Sports management software for the sports enthusiast</p>
+    <nav>
+        <ul>
+            <li><a href="/project_start/tech_support/">Home</a></li>
+        </ul>
+    </nav>
+</header>
