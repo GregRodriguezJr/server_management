a51dbbbb503496bc57f46b3a990da27bffbb676c
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-07-30 21:31:50 -0500

    up dated with try catch

--- ch15_tech_support/model/technician_db_oo.php
+++ ch15_tech_support/model/technician_db_oo.php
@@ -4,50 +4,68 @@ class TechnicianDB {
     public static function getTechnicians() {
         $db = Database::getDB();
 
-        $query = 'SELECT * FROM technicians
-                  ORDER BY lastName';
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $rows = $statement->fetchAll();
-        $statement->closeCursor();
-        
-        $technicians = array();
-        foreach($rows as $row) {
-            $t = new Technician(
-                    $row['firstName'], $row['lastName'],
-                    $row['email'], $row['phone'], $row['password']);
-            $t->setID($row['techID']);
-            $technicians[] = $t;
+        try {
+            $query = 'SELECT * FROM technician
+                      ORDER BY lastName';
+            $statement = $db->prepare($query);
+            $statement->execute();
+            $rows = $statement->fetchAll();
+            $statement->closeCursor();
+            
+            $technicians = array();
+            foreach($rows as $row) {
+                $t = new Technician(
+                        $row['firstName'], $row['lastName'],
+                        $row['email'], $row['phone'], $row['password']);
+                $t->setID($row['techID']);
+                $technicians[] = $t;
+            }
+            return $technicians;
+        } catch (PDOException $e) {
+            $error = "Database Error: " . $e->getMessage();
+            include('../errors/error.php');
+            exit();
         }
-        return $technicians;
     }
 
     public static function deleteTechnician($technician_id) {
         $db = Database::getDB();
 
-        $query = 'DELETE FROM technicians
-                 WHERE techID = :technician_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':technician_id', $technician_id);
-        $statement->execute();
-        $statement->closeCursor();
+        try {
+            $query = 'DELETE FROM technicians
+                     WHERE techID = :technician_id';
+            $statement = $db->prepare($query);
+            $statement->bindValue(':technician_id', $technician_id);
+            $statement->execute();
+            $statement->closeCursor();
+        } catch (PDOException $e) {
+            $error = "Database Error: " . $e->getMessage();
+            include('../errors/error.php');
+            exit();
+        }
     }
 
     public static function addTechnician($t) {
         $db = Database::getDB();
         
-        $query = 'INSERT INTO technicians
-                     (firstName, lastName, phone, email, password)
-                  VALUES
-                     (:first_name, :last_name, :phone, :email, :password)';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':first_name', $t->getFirstName());
-        $statement->bindValue(':last_name', $t->getLastName());
-        $statement->bindValue(':phone', $t->getPhone());
-        $statement->bindValue(':email', $t->getEmail());
-        $statement->bindValue(':password', $t->getPassword());
-        $statement->execute();
-        $statement->closeCursor();
+        try {
+            $query = 'INSERT INTO technicians
+                         (firstName, lastName, phone, email, password)
+                      VALUES
+                         (:first_name, :last_name, :phone, :email, :password)';
+            $statement = $db->prepare($query);
+            $statement->bindValue(':first_name', $t->getFirstName());
+            $statement->bindValue(':last_name', $t->getLastName());
+            $statement->bindValue(':phone', $t->getPhone());
+            $statement->bindValue(':email', $t->getEmail());
+            $statement->bindValue(':password', $t->getPassword());
+            $statement->execute();
+            $statement->closeCursor();
+        } catch (PDOException $e) {
+            $error = "Database Error: " . $e->getMessage();
+            include('../errors/error.php');
+            exit();
+        }
     }
 }
 ?>
