9988af80425cdce29ea14c356d40c3ac2f3eac04
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-07-30 20:34:50 -0500

    added try catch statements to data access functions for product db

--- ch15_tech_support/model/product_db.php
+++ ch15_tech_support/model/product_db.php
@@ -3,18 +3,25 @@ declare(strict_types=1);
 
 function get_products() {
     global $db;
-    $query = 'SELECT * FROM products
-              ORDER BY name';
-    $statement = $db->prepare($query);
-    $statement->execute();
-    $products = $statement->fetchAll();
-    $statement->closeCursor();
-    return $products;
+    try {
+        $query = 'SELECT * FROM products
+                  ORDER BY name';
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $products = $statement->fetchAll();
+        $statement->closeCursor();
+        return $products;
+    } catch (PDOException $e) {
+        $error = "Database Error: " . $e->getMessage();
+        include('../errors/error.php');
+        exit();
+    }
 }
 
 function get_products_by_customer($email) {
     global $db;
-    $query = 'SELECT products.productCode, products.name 
+    try {
+        $query = 'SELECT products.productCode, products.name 
               FROM products
                 INNER JOIN registrations ON products.productCode = registrations.productCode
                 INNER JOIN customers ON registrations.customerID = customers.customerID
@@ -25,58 +32,87 @@ function get_products_by_customer($email) {
     $products = $statement->fetchAll();
     $statement->closeCursor();
     return $products;
+    } catch (PDOException $e) {
+        $error = "Database Error: " . $e->getMessage();
+        include('../errors/error.php');
+        exit();
+    }
 }
 
 function get_product($product_code) {
     global $db;
-    $query = 'SELECT * FROM products
-              WHERE productCode = :product_code';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':product_code', $product_code);
-    $statement->execute();
-    $product = $statement->fetch();
-    $statement->closeCursor();
-    return $product;
+    try {
+        $query = 'SELECT * FROM products
+                  WHERE productCode = :product_code';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':product_code', $product_code);
+        $statement->execute();
+        $product = $statement->fetch();
+        $statement->closeCursor();
+        return $product;
+    } catch (PDOException $e) {
+        $error = "Database Error: " . $e->getMessage();
+        include('../errors/error.php');
+        exit();
+    }
 }
 
 function delete_product($product_code) {
     global $db;
-    $query = 'DELETE FROM products
-              WHERE productCode = :product_code';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':product_code', $product_code);
-    $statement->execute();
-    $statement->closeCursor();
+    try {
+        $query = 'DELETE FROM products
+                  WHERE productCode = :product_code';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':product_code', $product_code);
+        $statement->execute();
+        $statement->closeCursor();
+    } catch (PDOException $e) {
+        $error = "Database Error: " . $e->getMessage();
+        include('../errors/error.php');
+        exit();
+    }
 }
 
 function add_product(string $code, string $name, float $version, string $release_date) {
     global $db;
-    $query = 'INSERT INTO products
-                 (productCode, name, version, releaseDate)
-              VALUES
-                 (:code, :name, :version, :release_date)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':code', $code);
-    $statement->bindValue(':name', $name);
-    $statement->bindValue(':version', $version);
-    $statement->bindValue(':release_date', $release_date);
-    $statement->execute();
-    $statement->closeCursor();
+    try {
+        $query = 'INSERT INTO products
+                     (productCode, name, version, releaseDate)
+                  VALUES
+                     (:code, :name, :version, :release_date)';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':code', $code);
+        $statement->bindValue(':name', $name);
+        $statement->bindValue(':version', $version);
+        $statement->bindValue(':release_date', $release_date);
+        $statement->execute();
+        $statement->closeCursor();
+    } catch (PDOException $e) {
+        $error = "Database Error: " . $e->getMessage();
+        include('../errors/error.php');
+        exit();
+    }
 }
 
 function update_product(string $code, string $name, float $version, string $release_date) {
     global $db;
-    $query = 'UPDATE products
-              SET name = :name,
-                  version = :version,
-                  releaseDate = :release_date
-              WHERE productCode = :product_code';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':name', $name);
-    $statement->bindValue(':version', $version);
-    $statement->bindValue(':release_date', $release_date);
-    $statement->bindValue(':product_code', $code);
-    $statement->execute();
-    $statement->closeCursor();
+    try {
+        $query = 'UPDATE products
+                  SET name = :name,
+                      version = :version,
+                      releaseDate = :release_date
+                  WHERE productCode = :product_code';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':name', $name);
+        $statement->bindValue(':version', $version);
+        $statement->bindValue(':release_date', $release_date);
+        $statement->bindValue(':product_code', $code);
+        $statement->execute();
+        $statement->closeCursor();
+    } catch (PDOException $e) {
+        $error = "Database Error: " . $e->getMessage();
+        include('../errors/error.php');
+        exit();
+    }
 }
 ?>
