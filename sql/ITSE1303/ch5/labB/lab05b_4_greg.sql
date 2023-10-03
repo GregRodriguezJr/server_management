f5784220a0fbc2bd9ba3417d4f3df5c3ba4e6a09
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-10-02 21:25:12 -0500

    completed ch5 lab b

---
+++ sql/ITSE1303/ch5/labB/lab05b_4_greg.sql
@@ -0,0 +1,2 @@
+INSERT INTO products VALUES
+	(DEFAULT, 4, "dgx_640", "Yamaha DGX 640 88-Key Digital Piano", "Long description to come.", 799.99, 0, NOW());
---
+++ sql/ITSE1303/ch5/labB/lab05b_7_greg.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers
+	(customer_id, email_address, password, first_name, last_name, shipping_address_id, billing_address_id)
+VALUES
+	(DEFAULT, "rick@raven.com", "", "Rick", "Raven", NULL, NULL);
